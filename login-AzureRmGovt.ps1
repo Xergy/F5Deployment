344cd787cd3ef5639b479ddec56c47844bc67b35
Joe D. User Adm <joeuser-adm@agile.devops>
2018-06-22 13:25:01 -0400

    first commit

---
+++ login-AzureRmGovt.ps1
@@ -0,0 +1,13 @@
+﻿
+#if not logged in to Azure, start login
+if ((Get-AzureRmContext).Account -eq $Null) {
+Connect-AzureRmAccount -Environment AzureUSGovernment}
+
+#List all subs:
+Get-AzureRmSubscription
+
+#Choose a Sub
+
+Get-AzureRmSubscription | 
+    # Select-Object -Property Name,SubscriptionId,TenantId,State | 
+    Out-GridView -OutputMode Single -Title  "Choose a Subscription" | Set-AzureRmContext
